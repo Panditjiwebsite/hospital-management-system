04f784dad125de27cdffc87eedab258cc1aeac06
Panditjiwebsite <48588911+Panditjiwebsite@users.noreply.github.com>
2019-05-21 17:47:52 -0700

    May 22 Morning Start Project

---
+++ admin-panel.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Document</title>
+</head>
+<body>
+<div class="jumbotron">
+    <h1 class="text-center">Welcome Admin-Panel</h1>
+</div>
+<div class="container-flud">
+    <div class="row">
+        <div class="col-md-3">
+            <div class="list-group">
+                <a href="" class="list-group-item active">Patients</a>
+                <a href="" class="list-group-item">Patient Details</a>
+                <a href="" class="list-group-item">Add New Patient</a>
+                <a href="" class="list-group-item">Payment/Checkout</a>
+            </div>
+            <hr>
+            <div class="list-group">
+                <a href="" class="list-group-item active">Staff</a>
+                <a href="" class="list-group-item">Staff Details</a>
+                <a href="" class="list-group-item">Add New Staff</a>
+                <a href="" class="list-group-item">Remove Staff Member</a>
+            </div>
+        </div>
+        <div class="col-md-9">
+            <div class="card">
+                <div class="card-body admin-card-body"></div>
+                <div class="card-body">
+                    <form class="form-group" action="" method="post">
+                        <input type="text" name="" placeholder="User Name">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
